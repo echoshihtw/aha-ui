b382ccbeb062dbf71ba594ab44108742bfda4e93
Echoooo <echoshihtw@gmail.com>
2023-10-23 12:22:53 +0800

    chore: setup

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json', './tsconfig.json'],
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'arrow-body-style': [2, 'as-needed'],
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'object-curly-newline': 0,
+    quotes: [0, 'double'],
+    'react/function-component-definition': [
+      1,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{html,js,tsx}'],
+  theme: {
+    fontFamily: {
+      display: ['Ubuntu', 'cursive'],
+    },
+    screens: {
+      xs: '0px',
+      sm: '600px',
+      md: '1024px',
+      lg: '1440px',
+      xl: '1920px',
+    },
+    extend: {
+      fontSize: {
+        h3: ['48px', '72px'],
+        h4: ['30px', '45px'],
+        h5: ['24px', '36px'],
+        body1: ['16px', '24px'],
+        body2: ['14px', '21px'],
+        subtitle: ['16px', '24px'],
+      },
+      colors: {
+        tutor: {
+          main: '#FF9B33',
+        },
+        grey: {
+          400: '#B2B2B2',
+          500: '#929292',
+          600: '#6A6A6A',
+        },
+        background: {
+          light: '#1B1B1B',
+        },
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+};
