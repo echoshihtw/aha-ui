ef4c56c3648e404dd721392cad46edd6560bfd67
Echoooo <echoshihtw@gmail.com>
2023-10-29 16:01:42 +0800

    feat: remove comment

--- src/SideMenu/MobileMenu.tsx
+++ src/SideMenu/MobileMenu.tsx
@@ -6,7 +6,6 @@ const MobileMenu = () => (
   <nav className="h-[66px] sm:hidden grid place-items-center bg-background-light">
     <div className="flex gap-[50px]">
       {pages.map((page, index) => (
-        // eslint-disable-next-line react/no-array-index-key
         <SideMenuItem key={`${index}-${page.pageTitle}`} page={page} />
       ))}
     </div>
